aae284903f97493eeaa7bfa2eeb64a7e16cceb91
Peter Camilleri <peter.c.camilleri@gmail.com>
2015-09-03 16:05:02 -0400

    Added the middle bot.

---
+++ lib/connect_n_game/players/middle.rb
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+module ConnectNGame
+
+  #The EchoMoves echoes moves when possible.
+  class MiddleMoves < Player
+
+    #Build the player
+    def initialize(name = "Middle")
+      super(name, "Moves toward the middle", :silicon)
+    end
+
+    #Make a move. This bot prefers to play the middle.
+    #<br>Parameters
+    #* game - the game being played.
+    #* _piece - the piece to be played, 1 or 2. (Not used here)
+    #<br>Returns
+    #* A move, 1 .. rack.width
+    def make_move(game, _piece)
+      weights = []
+      game.rack.weights.each_with_index do |weight, index|
+        weights << [weight, index+1]
+      end
+
+      weights.sort.reverse_each do |weight, channel|
+        return channel unless game.rack.channel_full?(channel)
+      end
+    end
+
+    #The thrill of victory.
+    def winners_comments
+      "#{name} says 'Slow and steady wins the race!'"
+    end
+
+    #The agony of defeat
+    def losers_comments
+      "#{name} says 'Typical lunatic fringe playing I say!'"
+    end
+
+  end
+
+  Player.players << MiddleMoves.new
+end
