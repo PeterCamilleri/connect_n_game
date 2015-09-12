478c3fbcc370e9ec72d138c32829dd17c0c194bc
Peter Camilleri <peter.c.camilleri@gmail.com>
2015-09-12 17:58:33 -0400

    Added support for channels N thru Z.

--- lib/connect_n_game/util.rb
+++ lib/connect_n_game/util.rb
@@ -2,23 +2,25 @@
 
 #A small utility module
 module Utl
+  #An array with some letters in it and a place holder at zero.
+  Letters = %w(x A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
 
   #Convert a channel name to its number.
   #<br>Parameters
   #* letter - the letter that was played.
   #<br>Returns
-  #* The channel number (1..13) or nil if invalid.
+  #* The channel number (1..26) or nil if invalid.
   def self.name_to_channel(letter)
-    %w(x A B C D E F G H I J K L M).find_index(letter[0].upcase)
+    Letters.find_index(letter[0].upcase)
   end
 
   #Convert a channel number to its name.
   #<br>Parameters
-  #* channel - the channel number (1..13),
+  #* channel - the channel number (1..26),
   #<br>Returns
   #* The channel letter.
   def self.channel_to_name(channel)
-    %w(x A B C D E F G H I J K L M)[channel]
+    Letters[channel]
   end
 
 end
